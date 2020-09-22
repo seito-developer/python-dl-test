ded85b6b6468643f9eb67ed14cc7866fb20532fb
seito <seitokeri03@yahoo.co.jp>
2020-09-22 15:19:23 +0900

    updated

---
+++ tutorial/test.py
@@ -0,0 +1,88 @@
+#Comment
+# print("Hello World!")
+
+# print("海賊王に俺はなる")
+
+# print("モテたい")
+
+
+#演算
+# print(1+1)
+# print(1-1)
+# print(2*2)
+# print(10/2)
+# print(5%3)
+
+#変数
+# unko = 'l_size'
+# unko_length = 0
+# unko_times = 5.5
+
+# print(unko_length + unko_times)
+
+#条件分岐と関係演算子
+# if else elif
+# ==, !=, <, >, >=, <=
+# if unko_length > 6:
+# 	print('ooi')
+# elif unko_length == 0:
+# 	print('nai!')
+# else:
+# 	print('sukunai')
+
+# 関数
+# def unko_funbaru(arg):
+# 	unko_status = arg
+
+# 	if(unko_status < 10):
+# 		return 'mada daijobu'
+# 	else:
+# 		return 'yabai'
+
+# print()
+
+#list
+# unko_list = ['unko_small', 'unko_medium', 'unko_large']
+# print(unko_list[0])
+
+#for
+# for index in range(11):
+# 	print(unko_funbaru(index))
+
+# for item in unko_list:
+# 	print(item)
+
+#with
+#open()
+# with open('./unko.txt', 'r') as file:
+# 	print(file.read())
+
+#class
+
+# class Unko:
+# 	unko_size = 'l_size'
+# 	unko_length = 5
+# 	def unko_funbaru(arg):
+# 		unko_status = arg
+
+# 		if(unko_status < 10):
+# 			return 'mada daijobu'
+# 		else:
+# 			return 'yabai'
+
+#import
+import numpy
+
+numpy_list = [3, 1, 5, 10, 2093, 304, 123]
+print(numpy.sum(numpy_list))
+
+
+
+
+
+
+
+
+
+
+
--- vision_test/test.py
+++ vision_test/test.py
@@ -1,25 +1,20 @@
-import io
-import os
-
 # Imports the Google Cloud client library
 from google.cloud import vision
-from google.cloud.vision import types
 
 # Instantiates a client
 client = vision.ImageAnnotatorClient()
 
-# The name of the image file to annotate
-file_name = os.path.abspath('image.jpg')
-
 # Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
+with open('./image.jpg', 'rb') as image_file:
     content = image_file.read()
 
-image = types.Image(content=content)
+image = vision.types.Image(content=content)
 
 # Performs label detection on the image file
 response = client.label_detection(image=image)
 labels = response.label_annotations
 
+print('-----RESULT-----')
 for label in labels:
-    print(label)
+    print(label.description, ': ', label.score)
+print('-----RESULT-----')
